675128e01f075e81f465f578394cdaf7863d9e4d
xDark <aleshkailyashevich@gmail.com>
2019-04-19 17:06:44 +0300

    Remove commented code

--- src/main/java/org/feathercore/protogen/burger/BurgerItem.java
+++ src/main/java/org/feathercore/protogen/burger/BurgerItem.java
@@ -35,49 +35,6 @@ public class BurgerItem {
         this.id = json.get("numeric_id").getAsInt();
         this.enumName = textId.toUpperCase().replace(".", "_");
         this.maxStackSize = json.get("max_stack_size").getAsInt();
-        /*if (jar != null && methodName != null && desc != null) {
-            // Parse isSolid
-            JsonPrimitive primitive = json.getAsJsonPrimitive("class");
-            if (primitive != null) {
-                String className = primitive.getAsString();
-                if (className == null) {
-                    System.err.println("No class name for item: " + this.textId + "/" + this.id);
-                    return;
-                }
-                JarEntry entry = jar.getJarEntry(className.concat(".class"));
-                if (entry == null) {
-                    System.err.println("No class entry for item: " + this.textId + "/" + this.id + "/" + className);
-                    return;
-                }
-                byte[] buffer = new byte[256];
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                try (InputStream in = jar.getInputStream(entry)) {
-                    for (int r; ((r = in.read(buffer)) != -1); ) {
-                        baos.write(buffer, 0, r);
-                    }
-                } catch (IOException ex) {
-                    System.err.println(
-                            "Could not load class entry for item: " + this.textId + "/" + this.id + "/" + className);
-                    ex.printStackTrace();
-                    return;
-                }
-                ClassNode cn = new ClassNode();
-                new ClassReader(baos.toByteArray()).accept(cn, 0);
-                cn.methods.forEach(System.out::println);
-                for (MethodNode o : cn.methods) {
-                    if (methodName.equals(o.name) && desc.equals(o.name)) {
-                        InsnList list = o.instructions;
-                        for (AbstractInsnNode node : list.toArray()) {
-                            System.out.println(node);
-                        }
-                        break;
-                    }
-                }
-            }
-        } else if (!warned) {
-            System.err.println("No jar file or method name/descriptor was specified. isSolid will not be resolved");
-            warned = true;
-        }*/
     }
 
     public String getEnumName() {
